1e12b6cc6e86159348b25bc7c3ada5f83aee8903
AngieKCortes <cangie0722@gmail.com>
2024-05-06 21:32:24 -0500

    angiecortes

---
+++ Ejemplo1.java
@@ -0,0 +1,16 @@
+package ejemplo1;
+
+/**
+ *
+ * @author cangi
+ */
+public class Ejemplo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
